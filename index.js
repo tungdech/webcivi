39aea445ab416d45d418b2e1fe705907601c1e8c
tungdech <tstung94@gmail.com>
2018-07-11 00:11:59 +0700

    double accout

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const hostname = "0.0.0.0";
-const port = 80;
-const filePath = __dirname + "/accList";
-const myKey = "tung";
+const port = 8000;
 // Set bodyparser for using body in request
 const express = require("express");
 const app = express();
@@ -15,60 +13,90 @@ app.use(
     extended: true
   })
 );
-app.get("", function(req, res) {
+
+const thanhtung = {
+  name: 'thanktung',
+  // link: "/ha-thu-o-mat-ong-rung",
+  link: ".online",
+  filePath: __dirname + "/thanhtung123"
+}
+
+const sontung = {
+  name: 'sontung',
+  // link: "/ha-thu-o-mat-ong-rung-chua-bac-toc",
+  link: ".site",
+  filePath: __dirname + "/tungts123"
+}
+
+app.get("", function (req, res) {
   res.sendFile("Vien-Ha-Thu-O-Mat-Ong-Rung1.html", { root: __dirname });
 });
-router.post("/addInfo", function(req, res) {
-  var res = res;
-  var req = req;
+
+app.get(thanhtung.link, function (req, res) {
+  res.sendFile("Vien-Ha-Thu-O-Mat-Ong-Rung1.html", { root: __dirname });
+});
+
+app.get(sontung.link, function (req, res) {
+  res.sendFile("Vien-Ha-Thu-O-Mat-Ong-Rung1.html", { root: __dirname });
+});
+
+router.post("/addInfo", function (req, res) {
   console.log(req.body);
   if (!req.body) {
     res.send("error");
     return;
   }
+  var account = getAccount(req)
+  console.log("Account: " + account.name)
   res.send(req.body.name);
-  checkFile(filePath, function(isExist) {
+  checkFile(account.filePath, function (isExist) {
     // console.log(isExist + " exitst");
     let date = new Date();
     let time = new Date(date.valueOf() + 7 * 3600 * 1000);
     let line = time.toLocaleString() + ", " +
       req.body.name + ", " + req.body.phone + ", " + req.body.address + "</br>\n ";
     if (!isExist) {
-      fs.writeFile(filePath, "", "utf8", err => {
+      fs.writeFile(account.filePath, "", "utf8", err => {
         if (err) {
           console.log(err);
           return;
         }
-        fs.appendFileSync(filePath, line, "utf8");
+        fs.appendFileSync(account.filePath, line, "utf8");
       });
     } else {
-      fs.appendFileSync(filePath, line, "utf8");
+      fs.appendFileSync(account.filePath, line, "utf8");
     }
   });
 });
+
+function getAccount(req) {
+  if (thanhtung.link.indexOf(req.header.referer) !== -1) {
+    return thanhtung;
+  } else return sontung;
+}
 
 function checkFile(path, callback) {
   callback(fs.existsSync(path));
 }
 
-router.get("/get", function(req, res) {
+router.get("/get", function (req, res) {
   // console.log(req.query);
   let text = 'Khong co du lieu'
-  if (req.query.key == myKey) {
-    try{
-        text = fs.readFileSync(filePath, 'utf8').toString();
-    } catch(err) {
-    	res.send('Khong co du lieu')
+  let Account = getAccount(req)
+  // if (req.query.key == myKey) {
+    try {
+      text = fs.readFileSync(Account.filePath, 'utf8').toString();
+      res.send(text);
+    } catch (err) {
+      res.send('Khong co du lieu')
     }
-    res.send(text);
-
-  } else {
-    res.send("Sorry");
-  }
+  // } else {
+  //   res.send("Sorry");
+  // }
 });
 
-app.use("/", router);
+// app.use("/", router);
 
 app.listen(port, () => {
-   console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`Server running at http://${hostname}:${port}/`)
 });
